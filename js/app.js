e359c62660eba9928ccea4d9dcec3137f9f00313
Shadow <loud23for@gmail.com>
2026-04-20 11:48:14 +0800

    Initialize PoetryCollage system if defined

--- js/app.js
+++ js/app.js
@@ -53,7 +53,11 @@ async init() {
         if (typeof CharacterDetail !== 'undefined') {
             CharacterDetail.init();
         }
-        
+                // 6.5. 初始化拼贴诗系统
+        if (typeofPoetryCollage !== 'undefined') {
+            PoetryCollage.init();
+        }
+
         // 7. 绑定全局事件
         this.bindEvents();
         
